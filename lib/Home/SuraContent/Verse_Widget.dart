e38a8630cb620b2e087672bbbea838df2d36ecdc
MohamedAly002 <mo7amed3ly77@gmail.com>
2023-01-29 04:22:09 +0200

    + sura's content(files) %sura content to read file +verse widget

---
+++ lib/Home/SuraContent/Verse_Widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class VerseWidget extends StatelessWidget {
+  String content;
+  int verseindex;
+  VerseWidget(this.content,this.verseindex);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(vertical: 8),
+
+      child: Text('$content [$verseindex]',
+        textDirection: TextDirection.rtl,
+        textAlign: TextAlign.center,
+        style:TextStyle(fontSize: 18),),
+    );
+  }
+}
